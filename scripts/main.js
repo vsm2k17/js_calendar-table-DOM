0823dc033bcb1a324a0d4e760a81a6d0b1dbc232
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-14 16:56:07 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,14 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  const realMonth = month - 1;
-  const date = new Date(year, realMonth);
+  const currentMonth = month - 1;
+  const date = new Date(year, currentMonth);
   const firstDay = date.getDay() === 0 ? 7 : date.getDay();
   let maxDays = 31;
 
   date.setDate(31);
 
-  if (date.getMonth() !== realMonth) {
+  if (date.getMonth() !== currentMonth) {
     maxDays = 31 - date.getDate();
   }
 
@@ -22,37 +22,38 @@ function calendarTable(year, month, element) {
   calendar.append(table);
   table.append(tbody);
 
+  const daysAtWeek = 7;
+  const firstRow = 1;
+  const firstFullRow = 2;
   let counter = 1;
 
   for (let i = 0; counter <= maxDays; i++) {
     tbody.append(document.createElement('tr'));
 
-    for (let j = 0; j < 7; j++) {
+    for (let j = 0; j < daysAtWeek; j++) {
       if (i === 0) {
         tbody.rows[i].prepend(document.createElement('th'));
       } else {
         tbody.rows[i].append(document.createElement('td'));
       }
 
-      if (i === 1 && j >= firstDay - 1) {
+      if (i === firstRow && j >= firstDay - 1) {
         table.rows[i].cells[j].textContent = counter;
         counter++;
       }
 
-      if (i >= 2 && counter <= maxDays) {
+      if (i >= firstFullRow && counter <= maxDays) {
         table.rows[i].cells[j].innerHTML = counter;
         counter++;
       }
     }
   }
 
-  tbody.rows[0].cells[0].textContent = 'ПН';
-  tbody.rows[0].cells[1].textContent = 'ВТ';
-  tbody.rows[0].cells[2].textContent = 'СР';
-  tbody.rows[0].cells[3].textContent = 'ЧТ';
-  tbody.rows[0].cells[4].textContent = 'ПТ';
-  tbody.rows[0].cells[5].textContent = 'СБ';
-  tbody.rows[0].cells[6].textContent = 'ВС';
+  const namesOfDay = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+  for (let i = 0; i < namesOfDay.length; i++) {
+    tbody.rows[0].cells[i].textContent = namesOfDay[i];
+  }
 }
 
 calendarTable(2019, 10, calendar);
